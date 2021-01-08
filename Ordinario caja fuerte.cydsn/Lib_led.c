a188e8ed09ae2dca5f8b0a0a2c75d3fd14ce4622
Carlos Ivan Aguero <aguero_5209@hotmail.com>
2021-01-08 16:09:38 -0600

    Libreria led
    leds

---
+++ Ordinario caja fuerte.cydsn/Lib_led.c
@@ -0,0 +1,38 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* [] END OF FILE */
+
+#include "project.h"
+#include "Lib_led.h"
+
+
+/*static uint8 EstadoLeds=0x00;*/
+
+void PrenderVerde(void){
+      
+}
+void ApagarVerde(void){
+    
+}
+void PrenderAmarillo(void){
+    
+} 
+void ApagarAmarillo(void){
+    
+}
+void PrenderRojo(void){
+    
+}
+void ApagarRojo(void){
+    
+}
---
+++ Ordinario caja fuerte.cydsn/Lib_led.h
@@ -0,0 +1,33 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+/* [] END OF FILE */
+
+# ifndef _Lib_led_H_
+#define _Lib_led_H_
+
+/*#include <cytypes.h>    
+*# define PrimerLed 0x01
+*# define SegundoLed 0x02
+*# define TercerLed 0x04
+ */   
+extern void PrenderVerde(void);    
+extern void ApagarVerde(void);    
+
+extern void PrenderAmarillo(void);    
+extern void ApagarAmarillo(void);    
+
+extern void PrenderRojo(void);    
+extern void ApagarRojo(void);    
+
+    
+# endif
